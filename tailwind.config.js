c5eda296b4eaa374eae374f882caced2f1df6c0a
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2022-03-22 10:45:08 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				"plus-jakarta-sans": ["plus-jakarta-sans"],
+			},
+		},
+	},
+	plugins: [],
+};
